96e00d05f38a3b360bdc3d0481fce212f81e291c
Michael Crawford <cerealean@users.noreply.github.com>
2023-12-01 07:46:59 +0000

    Just playing around still

--- src/reader.ts
+++ src/reader.ts
@@ -1,13 +1,7 @@
-import * as fs from 'fs/promises';
-import { type Compendium, Type } from './compendium-types';
+import { compendium } from './compendium';
+import { Type } from './compendium-types';
 
-(async () => {
-    const json = await fs.readFile('./compendium.json');
-    const parsed = JSON.parse(json.toString()) as Compendium;
-
-    const ammunition = parsed.compendium.item
-        .filter(i => i.type && i.type === Type.A);
-    const groupedAmmo = Object.groupBy(ammunition, ({ detail }) => detail!);
-    console.log(ammunition.map(a => a.name));
-    // console.log(parsed.compendium.item.slice(0, 20));
-})();
+const ammunition = compendium.compendium.item
+    .filter(i => i.type && i.type === Type.A);
+const groupedAmmo = Object.groupBy(ammunition, ({ detail }) => detail as string);
+console.log(ammunition.slice(0, 10).map(a => a.name), groupedAmmo);
