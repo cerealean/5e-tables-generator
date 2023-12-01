d9953bf1be9c10fa42e9ff1aa051dc26aefbe2d6
Michael Crawford <cerealean@users.noreply.github.com>
2023-12-01 08:53:43 +0000

    check point

--- src/reader.ts
+++ src/reader.ts
@@ -1,7 +1,20 @@
+import * as mt19937 from '@stdlib/random-base-mt19937';
 import { compendium } from './compendium';
 import { Type } from './compendium-types';
 
+const getRandomIntInclusive = (min: number, max: number) => Math.floor(
+    (mt19937 as any).normalized()
+    *
+    (Math.floor(max) - Math.ceil(min) + 1)
+    +
+    min
+);
+
 const ammunition = compendium.compendium.item
-    .filter(i => i.type && i.type === Type.A);
-const groupedAmmo = Object.groupBy(ammunition, ({ detail }) => detail as string);
-console.log(ammunition.slice(0, 10).map(a => a.name), groupedAmmo);
+    .filter(i => i.type && i.type === Type.A)
+    .toSorted(() => getRandomIntInclusive(-1,1));
+
+for (let [index, rawr] of ammunition.entries()) {
+    console.info(index + 1, rawr.name);
+}
+// console.log(ammunition.slice(0, 10).map(a => a.name), groupedAmmo);
