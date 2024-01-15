6eb081b9442432689f95be22434750ae8965983b
Michael Crawford <cerealean@users.noreply.github.com>
2024-01-15 07:02:21 +0000

    Minor changes

--- src/reader.ts
+++ src/reader.ts
@@ -1,18 +1,17 @@
-import * as mt19937 from '@stdlib/random-base-mt19937';
 import { compendium } from './compendium';
-import { Type } from './compendium-types';
+import { DmgType, Type } from './compendium-types';
 
 const getRandomIntInclusive = (min: number, max: number) => Math.floor(
-    (mt19937 as any).normalized()
+    Math.random()
     *
     (Math.floor(max) - Math.ceil(min) + 1)
     +
     min
 );
 
-const ammunition = compendium.compendium.item
-    .filter(i => i.type && i.type === Type.A)
-    .toSorted(() => getRandomIntInclusive(-1,1));
+const ammunition = compendium.item
+    .filter(i => i.dmgType === DmgType.Necrotic)
+    .toSorted(() => getRandomIntInclusive(-1, 1));
 
 for (let [index, rawr] of ammunition.entries()) {
     console.info(index + 1, rawr.name);
